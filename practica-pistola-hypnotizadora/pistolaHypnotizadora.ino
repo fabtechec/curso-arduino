575981e7736677dbf19ae22ecbfa060b4db35b36
fabtechec <31801172+fabtechec@users.noreply.github.com>
2017-09-16 12:05:48 -0500

    Update pistolaHypnotizadora.ino

--- practica-pistola-hypnotizadora/pistolaHypnotizadora.ino
+++ practica-pistola-hypnotizadora/pistolaHypnotizadora.ino
@@ -1,17 +1,22 @@
-int piezoPin = A0;
-int motorPin = 13;
-int valorPiezo = 0;
+int PiezoPin = A0;
+int MotorPin = 13;
+int inputPiezo = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(motorPin,OUTPUT);
+  pinMode(MotorPin,OUTPUT);
+  pinMode(PiezoPin,INPUT)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  valorPiezo = analogRead(A0);
-  Serial.println(valorPiezo); 
-  valorPiezo = map(valorPiezo,300,1023,0,255);
-  analogWrite(motorPin,valorPiezo);
+  inputPiezo = analogRead(PiezoPin);
+  Serial.println(inputPiezo); 
+  if(inputPiezo > 0){
+    digitalWrite(MotorPin,HIGH);
+  }else{
+    digitalWrite(MotorPin,LOW);
+  }
+  delay(100);
 }
